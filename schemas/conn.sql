e92f74af38e0e6750a08221f49a123e4e71cc7c3
scottbelden <scottabelden@gmail.com>
2019-03-11 17:04:22 -0400

    change conn schema

--- schemas/conn.sql
+++ schemas/conn.sql
@@ -13,15 +13,12 @@ orig_bytes UInt64,
 resp_bytes UInt64,
 conn_state Enum8('OTH'=0, 'REJ'=1, 'RSTO'=2, 'RSTOS0'=3, 'RSTR'=4, 'RSTRH'=5, 'S0'=6, 'S1'=7, 'S2'=8, 'S3'=9, 'SF'=10, 'SH'=11, 'SHR'=12),
 local_orig Enum8('F'=0, 'T'=1),
-local_resp Enum8('F'=0, 'T'=1),
 missed_bytes UInt64,
 history String,
 orig_pkts UInt64,
 orig_ip_bytes UInt64,
 resp_pkts UInt64,
 resp_ip_bytes UInt64,
-orig_cc FixedString(2),
-resp_cc FixedString(2),
 peer String
 )
 ENGINE = MergeTree(day,sipHash64(uid), (day,sipHash64(uid), uid), 8192);
