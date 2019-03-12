ca73d0104c5bf76ebdeb278f2834b695e599a0ed
scottbelden <scottabelden@gmail.com>
2019-03-12 11:44:36 -0400

    check for table before creating it

--- create_table.py
+++ create_table.py
@@ -1,4 +1,6 @@
 from argparse import ArgumentParser
+import sys
+import re
 import requests
 
 parser = ArgumentParser(description="Run a SQL command to create a table")
@@ -8,6 +10,14 @@ args = parser.parse_args()
 
 with open(args.sql_file) as fp:
     query = fp.read()
+
+m = re.search(r"table (\S*)", query)
+table_name = m.group(1)
+
+tables = requests.post("http://localhost:8123", params={"query":"SHOW TABLES"}).text.split("\n")
+if table_name in tables:
+    print(f"Table `{table_name}` already exists, not continuing")
+    sys.exit()
 
 result = requests.post(
     "http://localhost:8123",
